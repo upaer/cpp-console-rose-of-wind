b8695533625dcf18c5bb58cb46649a5238ab97c3
upaer <154812817+upaer@users.noreply.github.com>
2024-01-04 14:17:45 +0300

    Добавлен вывод общей информации о задании

---
+++ main-project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #6. Rose of wind\n";
+    cout << "Author: Dmitry Mamoiko\n";
+    return 0;
+}
