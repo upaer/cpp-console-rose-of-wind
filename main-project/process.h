d722f86d3615fbc077fad21b0c421e684a908f4a
upaer <dimamamojko@gmail.com>
2024-01-23 18:30:08 +0300

    Довлена реализация тестов

--- main-project/process.h
+++ main-project/process.h
@@ -1,1 +1,5 @@
 #pragma once
+#include "rose-of-wind.h"
+
+
+float monthly_prepositions(wind_data* subscriptions[], int size, int month);
--- unit-test-project/unit-test-project.cpp
+++ unit-test-project/unit-test-project.cpp
@@ -1,16 +1,18 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../main-project/rose-of-wind.h"
+#include "../main-project/processing.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace unittestproject
 {
-	TEST_CLASS(unittestproject)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-		}
-	};
+    TEST_CLASS(unittestproject)
+    {
+    public:
+        TEST_METHOD(TestMethod1)
+        {
+            Assert::Fail();
+        }
+    };
 }
